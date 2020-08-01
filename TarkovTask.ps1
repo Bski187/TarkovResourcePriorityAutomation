fa4387a7061241b37ab196925133ff7ee3de2a9f
Bski187 <48719008+Bski187@users.noreply.github.com>
2020-08-01 20:24:34 +0200

    Add files via upload

---
+++ TarkovTask.ps1
@@ -0,0 +1,16 @@
+﻿# put this powershell file in your users\???\Documents\Escape From Tarkov folder
+# in scheduled tasks create a task and add the following:
+## type: powershell in program/script box
+### type: -ExecutionPolicy Bypass C:$env:HOMEPATH\Documents\Escape From Tarkov\TarkovTask.ps1 in the add parameters box
+#### in the trigger tab set it to trigger at logon and repeat every 5 minutes
+##### save and activate task if you want to use it right away
+
+function setTarkovPrio{
+    if(Get-Process EscapeFromTarkov)
+    {
+        (Get-Process -Name "EscapeFromTarkov").PriorityClass = "High";
+        (Get-Process -Name "EscapeFromTarkov_BE").PriorityClass = "High";
+        (Get-Process -Name "BsgLauncher").PriorityClass = "High";
+    }
+}
+setTarkovPrio;
